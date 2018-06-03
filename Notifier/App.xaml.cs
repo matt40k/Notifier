266095f0871519556cf3118910d5ae041f7bed4b
Matt Smith <matt@matt40k.uk>
2018-06-03 20:42:42 +0100

    Add author mode

--- Notifier/App.xaml.cs
+++ Notifier/App.xaml.cs
@@ -4,5 +4,15 @@ namespace Notifier
 {
     public partial class App : Application
     {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            bool AuthorMode = false;
+            if (e.Args.Length == 1)
+                if (e.Args[0].ToUpper() == "/AUTHOR")
+                    AuthorMode = true;
+
+            MainWindow wnd = new MainWindow(AuthorMode);
+            wnd.Show();
+        }
     }
 }
--- Notifier/MainWindow.xaml.cs
+++ Notifier/MainWindow.xaml.cs
@@ -1,39 +1,54 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Windows;
+using System.Windows.Media;
 using System.Windows.Media.Imaging;
 
 namespace Notifier
 {
     public partial class MainWindow : Window
     {
-        DateTime lastRaised;
-        FileSystemWatcher fs;
+        private DateTime lastRaised;
+        private FileSystemWatcher fs;
+        private bool _authorMode;
 
-        public MainWindow()
+        public MainWindow(bool AuthorMode)
         {
             InitializeComponent();
-            StartFileWatch();
+            if (!AuthorMode)
+                StartFileWatch();
+            else
+                EnableAuthorMode();
         }
 
-        private void okButton_Click(object sender, RoutedEventArgs e)
+        private void EnableAuthorMode()
         {
-            this.Visibility = Visibility.Hidden;
-        }
+            _authorMode = true;
 
-        private string ConnectionString
-        {
-            get
-            {
-                return Properties.Settings.Default.ConnectionString;
-            }
+            this.Visibility = Visibility.Visible;
+            this.Topmost = true;
+            this.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+            this.WindowStyle = WindowStyle.ThreeDBorderWindow;
+            this.ShowInTaskbar = true;
+
+            this.messageView.Visibility = Visibility.Hidden;
+            this.messageEdit.Visibility = Visibility.Visible;
+
+            this.okButton.Content = "Send";
+
+            this.NotifierType.ItemsSource = GetImages;
+            this.NotifierType.SelectedIndex = 0;
+            this.NotifierType.Items.Refresh();
+            this.NotifierType.Visibility = Visibility.Visible;
         }
 
         private void StartFileWatch()
         {
+            _authorMode = false;
             if (Directory.Exists(ConnectionString))
             {
-                fs = new FileSystemWatcher(ConnectionString, "*.*");
+                fs = new FileSystemWatcher(ConnectionString, ("*" + MessageExtension));
                 fs.EnableRaisingEvents = true;
                 fs.IncludeSubdirectories = false;
                 fs.Created += new FileSystemEventHandler(newfile);
@@ -49,7 +64,7 @@ namespace Notifier
                     string newMessagePath = Path.Combine(ConnectionString, Eventocc.Name);
                     FileInfo createdFile = new FileInfo(newMessagePath);
                     string extension = createdFile.Extension;
-                    if (extension == ".txt")
+                    if (extension == MessageExtension)
                     {
                         string imageName = null;
 
@@ -63,11 +78,12 @@ namespace Notifier
                         string newMessage = File.ReadAllText(newMessagePath);
                         this.Dispatcher.Invoke(() =>
                         {
-                            messageBlock.Text = newMessage;
+                            this.messageView.Visibility = Visibility.Visible;
+                            this.messageEdit.Visibility = Visibility.Hidden;
+                            messageView.Text = newMessage;
                             this.Visibility = Visibility.Visible;
                             this.Topmost = true;
                             this.WindowStartupLocation = WindowStartupLocation.CenterScreen;
-
 
                             string imgPath = ImagePath(imageName);
                             if (!string.IsNullOrWhiteSpace(imgPath))
@@ -90,7 +106,7 @@ namespace Notifier
         {
             if (!string.IsNullOrWhiteSpace(name))
             {
-                string filePath = name + ".png";
+                string filePath = name + ImageExtension;
                 if (File.Exists(filePath))
                     return filePath;
                 filePath = Path.Combine(ConnectionString, filePath);
@@ -102,12 +118,111 @@ namespace Notifier
 
         private BitmapImage ImageData(string path)
         {
-            BitmapImage img = new BitmapImage();
-            img.BeginInit();
-            img.UriSource = new Uri(path);
-            img.DecodePixelWidth = 280;
-            img.EndInit();
-            return img;
+            if (!string.IsNullOrWhiteSpace(path) && File.Exists(path))
+            {
+                BitmapImage img = new BitmapImage();
+                img.BeginInit();
+                img.UriSource = new Uri(path);
+                img.DecodePixelWidth = 280;
+                img.EndInit();
+                return img;
+            }
+            return null;
+        }
+
+        private void NotifierType_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            if (this.NotifierType.SelectedValue != null)
+            {
+                string imgPath = Path.Combine(ConnectionString, (GetNotifierType + ImageExtension));
+                this.image.Source = ImageData(imgPath);
+            }
+        }
+
+        private string GetNotifierType
+        {
+            get
+            {
+                if (this.NotifierType.SelectedValue != null)
+                {
+                    string type = this.NotifierType.SelectedValue.ToString();
+                    if (type == "None")
+                        return null;
+                    else
+                        return type;
+                }
+                return null;
+            }
+        }
+
+        private List<string> GetImages
+        {
+            get
+            {
+                List<string> imgs = new List<string>();
+                imgs.Add("None");
+                var images = Directory.GetFiles(ConnectionString, ("*" + ImageExtension), SearchOption.TopDirectoryOnly);
+                foreach (var image in images)
+                {
+                    imgs.Add(Path.GetFileNameWithoutExtension(image));
+                }
+                return imgs;
+            }
+        }
+
+        private string ImageExtension
+        {
+            get
+            {
+                return ".png";
+            }
+        }
+
+        private string MessageExtension
+        {
+            get
+            {
+                return ".txt";
+            }
+        }
+
+        private void okButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (_authorMode)
+                SendMessage();
+            else
+                this.Visibility = Visibility.Hidden;
+        }
+
+        private string ConnectionString
+        {
+            get
+            {
+                return Properties.Settings.Default.ConnectionString;
+            }
+        }
+
+        private void SendMessage()
+        {
+            string user = (Environment.UserName).Replace("\\", "_");
+            string dt = DateTime.Now.ToString("_yyyyMMdd_HHmmss");
+            string messageFileName = user + dt + MessageExtension;
+
+            string type = GetNotifierType;
+            if (!string.IsNullOrWhiteSpace(type))
+                messageFileName = type + "_" + messageFileName;
+
+            string messageFilePath = Path.Combine(ConnectionString, messageFileName);
+            try
+            {
+                File.WriteAllText(messageFilePath, messageEdit.Text);
+            }
+            catch(Exception)
+            {
+
+            }
+
+            Application.Current.Shutdown();
         }
     }
 }
